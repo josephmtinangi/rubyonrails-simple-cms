c985f03bca7f252b29a9d29f51e830a033575899
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 19:25:09 +0300

    Add shorthand column definition

--- db/migrate/20161104161709_create_users.rb
+++ db/migrate/20161104161709_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def up
     create_table :users do |t|
       t.column "first_name", :string
-      
+      t.string "last_name"
 
       t.timestamps
     end
