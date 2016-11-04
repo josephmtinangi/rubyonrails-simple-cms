a98b14a781afad53962852b5564e1b24353aebf7
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 19:28:37 +0300

    Add cool timestamps (Required for each table)

--- db/migrate/20161104161709_create_users.rb
+++ db/migrate/20161104161709_create_users.rb
@@ -7,7 +7,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string "email", :default => '', :null => false
       t.string "password", :limit => 40
 
-
+      t.datetime "created_at"
+      t.datetime "updated_at"
 
       t.timestamps
     end
