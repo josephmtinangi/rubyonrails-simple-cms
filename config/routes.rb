4e208a88da6963eeeb00a41dfb972fc34f22eb69
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 17:07:59 +0300

    Just use the simple route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # get 'demo/index'
+  # Simple route
+  get 'demo/index'
+
   # Default route
-  get ':controller(/:action(/:id))'
+  # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
