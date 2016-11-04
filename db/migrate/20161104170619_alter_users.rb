193557ca4c543c1b9de772e0908f856b9714f13e
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 20:31:45 +0300

    Bug fix

--- db/migrate/20161104170619_alter_users.rb
+++ db/migrate/20161104170619_alter_users.rb
@@ -3,13 +3,13 @@ class AlterUsers < ActiveRecord::Migration[5.0]
     rename_table("users", "admin_users")
     add_column("admin_users", "username", :string, :limit => 25, :after => "email")
     change_column("admin_users", "email", :string, :limit => 100)
-    change_column("admin_users", "password", "hashed_password")
+    rename_column("admin_users", "password", "hashed_password")
     puts "*** Adding an index ***"
     add_index("admin_users", "username")
   end
   def down
     remove_index("admin_users", "username")
-    change_column("admin_users", "hashed_password", "password")
+    rename_column("admin_users", "hashed_password", "password")
     change_column("admin_users", "email", :string, :default => '')
     remove_column("admin_users", "username", :string, :limit => 25, :after => "email")
     rename_table("admin_users", "users")
