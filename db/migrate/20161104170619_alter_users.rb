22581d0b5673d50179f888985c8d0672fce1d237
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 20:21:30 +0300

    Add column

--- db/migrate/20161104170619_alter_users.rb
+++ db/migrate/20161104170619_alter_users.rb
@@ -1,6 +1,7 @@
 class AlterUsers < ActiveRecord::Migration[5.0]
   def up
     rename_table("users", "admin_users")
+    add_column("admin_users", "username", :string, :limit => 25, :after => "email")
   end
   def down
   end
