1123f0afca14303b4d7f26e8630648fc21711250
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 17:07:04 +0300

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'demo/index'
-
+  # get 'demo/index'
+  # Default route
+  get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
