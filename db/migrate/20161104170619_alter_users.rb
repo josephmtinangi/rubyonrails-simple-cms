fb829b96bd1b941d224346357b29cb606195202f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-04 20:23:21 +0300

    Rename column

--- db/migrate/20161104170619_alter_users.rb
+++ db/migrate/20161104170619_alter_users.rb
@@ -2,6 +2,8 @@ class AlterUsers < ActiveRecord::Migration[5.0]
   def up
     rename_table("users", "admin_users")
     add_column("admin_users", "username", :string, :limit => 25, :after => "email")
+    change_column("admin_users", "email", :string, :limit => 100)
+    change_column("admin_users", "password", "hashed_password")
   end
   def down
   end
