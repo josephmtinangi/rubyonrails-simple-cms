783b3fdd9135a55cdf875966880c345e17a71c9f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-05 19:01:40 +0300

    belongs_to presence validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,6 @@ class Page < ApplicationRecord
 
   belongs_to :subject
   has_many :sections
-  belongs_to :subject, { :foreign_key => 'subject_id' }
+  belongs_to :subject, { :foreign_key => 'subject_id' }, { :optional => false }
 
 end
