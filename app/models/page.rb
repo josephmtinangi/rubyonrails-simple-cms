3539054ff231e84c56ddb1c98d485b38ac3f1f5f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-05 19:18:07 +0300

    Associate models

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,5 +3,6 @@ class Page < ApplicationRecord
   belongs_to :subject
   has_many :sections
   belongs_to :subject, { :foreign_key => 'subject_id' }, { :optional => false }
+  has_and_belongs_to_many :admin_users
 
 end
