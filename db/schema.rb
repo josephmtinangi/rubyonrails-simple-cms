7a2028be85dba4a23eb796025fc48077fde7a538
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-05 19:16:27 +0300

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104212129) do
+ActiveRecord::Schema.define(version: 20161105161106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20161104212129) do
     t.datetime "updated_at",                               null: false
     t.string   "username",        limit: 25
     t.index ["username"], name: "index_admin_users_on_username", using: :btree
+  end
+
+  create_table "admin_users_pages", id: false, force: :cascade do |t|
+    t.integer "admin_user_id"
+    t.integer "page_id"
+    t.index ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id", using: :btree
   end
 
   create_table "pages", force: :cascade do |t|
