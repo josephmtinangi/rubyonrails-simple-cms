e57b3ab58084855f99a89654e169bcb78c66f728
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-05 23:35:45 +0300

    Create flash messages

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -17,6 +17,7 @@ class SubjectsController < ApplicationController
   def create
     @subject = Subject.new(subject_params)
     if @subject.save
+      flash[:notice] = "Subject created successfully."
       redirect_to(subjects_path)
     else
       render "new"
@@ -31,6 +32,7 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
 
     if @subject.update_attributes(subject_params)
+      flash[:notice] = "Subject updated successfully."
       redirect_to(subject_path(@subject))
     else
       render "edit"
@@ -44,6 +46,7 @@ class SubjectsController < ApplicationController
   def destroy
     @subject = Subject.find(params[:id])
     @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}' destroyed successfully."
     redirect_to(subjects_path)
   end
 
